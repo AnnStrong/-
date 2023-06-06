0e87eedf93892eac7b81e307db25b41d3c9c336e
Anna <happyz2104@gmail.com>
2023-06-06 19:03:04 +0300

    ДЗ 4

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿int number = ReadLine();
+int sum = Sum(number);
+Console.WriteLine(sum);
+
+int Sum(int a)
+{
+int sum = 0;
+for (int i = 0; i <= a; i++)
+{
+sum += i;
+}
+return sum;
+}
+
+int ReadLine()
+{
+Console.WriteLine("Введите число");
+string input = Console.ReadLine();
+int number = Convert.ToInt32(input);
+return number;
+}
